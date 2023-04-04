4750c76d3f13f53ed6659c7b15a7603f7117ff59
Alexandr Dobrin <dobrin.alexandrr@gmail.com>
2023-04-04 20:38:30 +0300

    refactor: change project structure for typescript
    Split project into src and dist directories for future rewrite on
    typescript
    
    Copy scripts from src to dist so can be changed safely
    
    Add tsconfig

---
+++ src/scripts/controls/TableControlsLayout.js
@@ -0,0 +1,155 @@
+export class TableControlsLayout {
+    addRowControl = null;
+    removeRowControl = null;
+    addColControl = null;
+    removeColControl = null;
+    tableOffset = null;
+
+    pointerTriggerRadius = null;
+    table = null;
+    clientOffset = null;
+
+    controlDimensions = 40; 
+    pointerEvent = null;
+    tableCoords = null;
+    cellUnderPointerCoords = null;
+
+    constructor(
+        addRowControl,
+        removeRowControl,
+        addColControl,
+        removeColControl,
+        table,
+        tableOffset
+    ) {
+        this.addRowControl = addRowControl;
+        this.removeRowControl = removeRowControl;
+        this.addColControl = addColControl;
+        this.removeColControl = removeColControl;
+        this.tableOffset = tableOffset;
+
+        this.pointerTriggerRadius = [ 
+            { x: 0, y: 0 },
+            { x: this.tableOffset, y: 0 },
+            { x: -this.tableOffset, y: 0 },
+            { x: 0, y: this.tableOffset },
+            { x: 0, y: -this.tableOffset },
+            
+            { x: this.tableOffset, y: this.tableOffset },
+            { x: -this.tableOffset, y: -this.tableOffset },
+            { x: -this.tableOffset, y: this.tableOffset },
+            { x: this.tableOffset, y: -this.tableOffset },
+        ];
+
+        this.table = table;
+
+        this.clientOffset = Math.max(
+            this.table.$table.firstElementChild.offsetLeft,
+            this.table.$table.firstElementChild.offsetTop,
+        );
+
+        document.addEventListener('controlPressed', () => this.onControlPressed());
+    }
+
+    onControlPressed() {
+        if (this.table.rowsLength <= 1) {
+            this.removeRowControl.hide();
+        }
+        if (this.table.colsLength <= 1) {
+            this.removeColControl.hide();
+        }
+    }
+
+    hideControls() {
+        this.hideCreationControls();
+        this.removeRowControl.hide();
+        this.removeColControl.hide();
+    }
+
+    hideCreationControls() {
+        this.addRowControl.hide();
+        this.addColControl.hide();
+    }
+
+    showControls() {
+        this.addRowControl.show();
+        this.addColControl.show();
+        if (this.table.rowsLength > 1) {
+            this.removeRowControl.show();
+        }
+        if (this.table.colsLength > 1) {
+            this.removeColControl.show();
+        }
+    }
+
+    layout(event) {
+        this.pointerEvent = event;
+        this.tableCoords = this.table.$table.getBoundingClientRect();
+
+        if (this.isPointerOnTable()) {
+            this.layoutInTableCoords();
+            return;
+        }
+
+        this.layoutInAreaCoords();
+    }
+
+    isPointerOnTable() {
+        return this.pointerEvent.pageX > this.tableCoords.left &&
+            this.pointerEvent.pageY > this.tableCoords.top &&
+            this.pointerEvent.pageX < this.tableCoords.right &&
+            this.pointerEvent.pageY < this.tableCoords.bottom;
+    }
+
+    layoutInTableCoords() {
+        if (this.pointerEvent.target.tagName !== 'TD') {
+            return;
+        }
+
+        this.tableCoords = this.table.$table.getBoundingClientRect();
+        this.cellUnderPointerCoords = this.pointerEvent.target.getBoundingClientRect();
+        this.layoutControls();
+        this.showControls();
+    }
+
+    layoutInAreaCoords() {
+        const closestCell = this.getPointerClosestCell();
+        if (closestCell?.tagName !== 'TD') {
+            return;
+        }
+        this.cellUnderPointerCoords = closestCell.getBoundingClientRect();
+        this.layoutControls();
+        this.showControls();
+    }
+
+    getPointerClosestCell() {
+        let $closestCell = null;
+        for (const { x, y } of this.pointerTriggerRadius) {
+            const $cell = document.elementFromPoint(this.pointerEvent.pageX + x, this.pointerEvent.pageY + y);
+            if ($cell?.tagName === 'TD') {
+                $closestCell = $cell;
+                break;
+            }
+        }
+        return $closestCell;
+    }
+
+    layoutControls() {
+        this.layoutCreationControls();
+        this.layoutDestructionControls();
+    }
+
+    layoutCreationControls() {
+        const y = this.tableCoords.bottom + this.clientOffset;
+        this.addRowControl.setPosition(this.cellUnderPointerCoords.left, y);
+        const x = this.tableCoords.right + this.clientOffset;
+        this.addColControl.setPosition(x, this.cellUnderPointerCoords.top);
+    }
+
+    layoutDestructionControls() {
+        const x = this.tableCoords.left - (this.controlDimensions + this.clientOffset);
+        this.removeRowControl.setPosition(x, this.cellUnderPointerCoords.top);
+        const y = this.tableCoords.top - (this.controlDimensions + this.clientOffset);
+        this.removeColControl.setPosition(this.cellUnderPointerCoords.left, y);
+    }
+}
---
+++ src/scripts/table/TableObserver.js
@@ -0,0 +1,67 @@
+export class TableObserver {
+    $table = null;
+    controlsLayout = null;
+    observeAreaOffset = null;
+
+    mutationObserver = null;
+
+    observeAreaCoords = null;
+
+    constructor($table, controlsLayout, observeAreaOffset) {
+        this.$table = $table;
+        this.controlsLayout = controlsLayout;
+        this.observeAreaOffset = observeAreaOffset;
+
+        this.mutationObserver = new MutationObserver(event => this.onStructureChanged(event));
+        this.mutationObserver.observe(this.$table, { childList: true, subtree: true });
+
+        this.calcObserveArea();
+
+        document.addEventListener(
+            'pointermove',
+            event => this.pointerEventHandler(event)
+        );
+
+        document.addEventListener(
+            'pointerup',
+            event => {
+                if (!event.target.classList.contains('table-control'))
+                    return;
+                this.pointerEventHandler(event);
+            }
+        );
+    }
+
+    pointerEventHandler(event) {
+        if (!this.isPointerInObserveArea(event)) {
+            this.controlsLayout.hideControls();
+            return;
+        }
+        this.controlsLayout.layout(event);
+    }
+
+    calcObserveArea() {
+        const tableCoords = this.$table.getBoundingClientRect();
+        this.observeAreaCoords = {
+            top: tableCoords.top - this.observeAreaOffset,
+            left: tableCoords.left - this.observeAreaOffset,
+            right: tableCoords.right + this.observeAreaOffset,
+            bottom: tableCoords.bottom + this.observeAreaOffset,
+        };
+    }
+
+    isPointerInObserveArea(pointer) {
+        return pointer.pageX > this.observeAreaCoords.left &&
+            pointer.pageX < this.observeAreaCoords.right &&
+            pointer.pageY > this.observeAreaCoords.top &&
+            pointer.pageY < this.observeAreaCoords.bottom;
+    }
+
+    onStructureChanged(mutation) {
+        this.calcObserveArea();
+        if (mutation[0].addedNodes.length === 0) {
+            return;
+        }
+        this.controlsLayout.hideCreationControls();
+    }
+}
